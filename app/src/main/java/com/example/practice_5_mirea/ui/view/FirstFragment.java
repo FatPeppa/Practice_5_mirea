caa7ffcc1a86c10aac281fa9b03b672aaa8f405c
Daniil Popov <popovdanin@gmail.com>
2024-04-17 23:53:49 +0300

    Practice 11 task 1 changes commit 2 and task 2 changes commit 1

--- app/src/main/java/com/example/practice_5_mirea/ui/view/FirstFragment.java
+++ app/src/main/java/com/example/practice_5_mirea/ui/view/FirstFragment.java
@@ -3,10 +3,14 @@ package com.example.practice_5_mirea.ui.view;
 import static com.example.practice_5_mirea.domain.InputsValidator.checkGoodAmount;
 import static com.example.practice_5_mirea.domain.InputsValidator.checkGoodName;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +25,7 @@ import com.example.practice_5_mirea.domain.InputsValidator;
 import com.example.practice_5_mirea.ui.viewModels.ProductViewModel;
 
 public class FirstFragment extends Fragment {
+    ImageView imageView;
     Button first_fragment_button;
     EditText first_fragment_edit_text1;
     EditText first_fragment_edit_text2;
@@ -36,9 +41,12 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        first_fragment_button = (Button) getActivity().findViewById(R.id.fragment_first_button);
         first_fragment_edit_text1 = (EditText) getActivity().findViewById(R.id.fragment_first_edit_text1);
         first_fragment_edit_text2 = (EditText) getActivity().findViewById(R.id.fragment_first_edit_text2);
+        imageView = (ImageView) getActivity().findViewById(R.id.imageView);
+        AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) imageView.getDrawable();
+        drawable.reset();
+
         ProductViewModel productViewModel = new ViewModelProvider(getActivity()).get(ProductViewModel.class);
         productViewModel.getUiState().getValue().createCurrentInfoKeeper(getActivity().getApplicationContext());
 
@@ -63,18 +71,22 @@ public class FirstFragment extends Fragment {
             }
         });
 
-        first_fragment_button.setOnClickListener(new View.OnClickListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 String good_name = first_fragment_edit_text1.getText().toString();
                 String good_amount = first_fragment_edit_text2.getText().toString();
 
                 if (checkGoodName(good_name)) {
-
                     if (checkGoodAmount(good_amount)) {
-
-                        productViewModel.inputGoodParameters(good_name, good_amount);
-                        Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment);
+                        drawable.start();
+                        view.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                productViewModel.inputGoodParameters(good_name, good_amount);
+                                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment);
+                            }
+                        }, 400);
                     } else {
                         first_fragment_edit_text2.setText("");
                         first_fragment_edit_text2.setHint(getResources().getString(R.string.fragment_first_egit_text_hint2));
